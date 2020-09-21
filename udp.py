d470a6642774681ecad92b4dca9ceb59581ca291
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2020-09-21 15:55:35 +0900

    Deleted c3/ d3/ Moved c3.css c3.min.js d3.min.js to Home

--- udp.py
+++ udp.py
@@ -3,13 +3,15 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 
 HOST = ''   
-PORT = 5001
+PORT = 5002
 
 s = socket(AF_INET, SOCK_DGRAM)
 s.bind((HOST, PORT))
 
 while True:
-    msg, address = s.recvfrom(8192)
-    print "message: ",msg,"from: ",address
+	msg, address = s.recvfrom(256)
+	val=eval(msg)
+	for k in val: val[k]=int(val[k])
+	print(val)
 
 s.close()
